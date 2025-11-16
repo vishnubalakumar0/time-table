2bec7adf1d71c9b06d1bcfd7a5f7117ff4b0abd5
vishnubalakumar0 <vishnubalakumar15@gmail.com>
2025-11-16 18:20:43 +0530

    finale

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,9 @@
-// Import Firebase SDK
+// src/utils/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyCKZbhoyJ-sE2B2PBF3L2BfjwjBI81k744",
   authDomain: "timetable-1c2c6.firebaseapp.com",
@@ -11,12 +11,11 @@ const firebaseConfig = {
   storageBucket: "timetable-1c2c6.firebasestorage.app",
   messagingSenderId: "157226793900",
   appId: "1:157226793900:web:d8d7f39b423e0bdf339809",
-  measurementId: "G-3GWGVFNYLQ"
+  measurementId: "G-3GWGVFNYLQ",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Auth & Firestore so we can use them in the app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export default app;
